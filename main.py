e88829b87b46e040c3972199f4625f11fc6ee42c
aberiggs <34726295+aberiggs@users.noreply.github.com>
2019-06-23 21:26:48 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,38 @@ import pyfiglet
 from pyfiglet import Figlet
 
 def menu():
-    main_font = Figlet(font="poison")
-    banner = main_font.renderText("Weeb - Sec")
-    print(banner)
+    valid_input = False
+    while True:
+        print("\n TOOLS")
+        print("=======")
+        tools = ["autism", "dead"]
+        for i in range(len(tools)):
+            print(str(i+1)+ ") " + tools[i])
+            
+        try:
+            selection = int(input("\n\nPlease select the # of the " \
+                            + "tool that you would like to use\n    "))
+            if selection >= len(tools)+1 or selection <= 0:
+                print("\nERROR: Invalid tool #\n\n")
+                valid_input = False
+            else:
+                valid_input = True
+        except ValueError:
+            print("\nERROR: Numeric values only\n\n")
+        if valid_input == True:
+            break
+            
 
-def selections():
-    exit()
+
+    print("\nyou did it retard")
 
 def main():
-    menu()
-    test = input("")
+    main_font = Figlet(font="banner3") #Setup ASCII font
+    banner = main_font.renderText("Weeb | Sec") #Create banner
+    print(banner)
+
+    selection = menu()
+    
+    
 
 main()
